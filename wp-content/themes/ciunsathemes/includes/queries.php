c8447e582ad604e313cf590e11d697e61ff2e705
matiferamador <107516034+matiferamador@users.noreply.github.com>
2023-12-18 08:32:27 -0300

    guardar cambios

--- wp-content/themes/ciunsathemes/includes/queries.php
+++ wp-content/themes/ciunsathemes/includes/queries.php
@@ -1,6 +1,5 @@
 <?php 
 function ciunsa_lista_revista($cantidad = -1) {
-
     ?>
     <ul class=" listado-grid">
                 <?php
@@ -30,7 +29,6 @@ function ciunsa_lista_revista($cantidad = -1) {
                 ?>
             </ul>
     <?php
-
 }
 function ciunsa_novedades_destacadas() {
     ?>
@@ -38,25 +36,19 @@ function ciunsa_novedades_destacadas() {
         <?php 
         $args = array(
             'post_type' => 'novedades_destacadas',
-            'order'          => 'ASC',       // En orden ascendente (del más antiguo al más reciente)
-                  
+            'order'          => 'ASC',       // En orden ascendente (del más antiguo al más reciente)      
         );
         $novedadesDestacadas = new WP_Query($args);
-
         while ($novedadesDestacadas->have_posts()) {
-            $novedadesDestacadas->the_post();
-            
+            $novedadesDestacadas->the_post(); 
             // Obtén la URL de la imagen destacada
             $thumbnail_url = get_the_post_thumbnail_url();
             ?>
             <div style="background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('<?php echo $thumbnail_url; ?>');" class="novedadesDestacadas text-center swiper-slide">
                 <div class="content">
-                
                         <h2>
                         <?php the_title(); ?>
                         </h2>
-                
-                <!-- <p><?php the_excerpt(); ?></p> -->
                 <div class="contenedor-boton">
                     <a href="<?php the_permalink() ?>" class="boton boton-primario ">Más Información</a>
                 </div>
@@ -64,7 +56,6 @@ function ciunsa_novedades_destacadas() {
             </div>
             <?php 
         }
-
         wp_reset_postdata();
         ?>
     </div>
